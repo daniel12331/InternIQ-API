fdb32a1e58875a1069e747cd85ace33fdc0e3db3
Daniel <20088708@wit.ie>
2023-02-13 23:35:13 +0000

    Initial Structure, User model + Job Model completed + validation and error checking complete.

---
+++ index.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+require('express-async-errors');
+
+
+const rateLimiter = require('express-rate-limit');
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/index');
+const authenticateUser = require('./middleware/authentication');
+// routers
+const userRouter = require('./routes/user');
+const jobsRouter = require('./routes/jobs');
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(express.json());
+
+
+
+// routes
+app.use('/api/user', userRouter);
+app.use('/api/jobs', authenticateUser, jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_DB);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
